1e09585e8146f2832a73c54414a7b1656ed72dd3
Ahmed Chabrak <ahmed.chebrak@gmail.com>
2025-12-14 12:04:10 +0100

    Clarify violation comment in DataManager class
    Updated comment to clarify class violation.

--- src/main/java/com/interview/util/DataManager.java
+++ src/main/java/com/interview/util/DataManager.java
@@ -10,7 +10,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-// VIOLATION: God class that does everything
+// VIOLATION: class that does everything
 // VIOLATION: Mixing JPA and JDBC
 // VIOLATION: Business logic in utility class
 @Component
